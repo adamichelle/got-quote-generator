66c7f114726521826652fa687540a37017ba82ba
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-05-14 00:33:44 +0100

    create database helper functions to help us manipulate data

---
+++ js/dbHelper.js
@@ -0,0 +1,50 @@
+/**
+ * Helper functions to access our data
+ */
+export default class DBHelper {
+    /** Get Databse URL 
+     * (in this case our data.json file) */
+    static get DATABASE_URL() {
+        return `./data/quotes.json`;
+    }
+
+    static fetchQuotes(callback) {
+        fetch(DBHelper.DATABASE_URL)
+        .then(response => response.json())
+        .then(quotes => {
+            console.log("Getting quotes from API");
+
+            callback(null, quotes);
+        })
+        .catch((e) => console.log(e))
+    }
+
+    static getRandomQuote(quotesArray) {
+        let quotesCopy = quotesArray.slice(0);
+        
+        return function() {
+          if (quotesCopy.length < 1) {
+            quotesCopy = quotesArray.slice(0);
+          }
+          let index = Math.floor(Math.random() * quotesCopy.length);
+          let singleQuoteArray = quotesCopy[index];
+          quotesCopy.splice(index, 1);
+          return singleQuoteArray;
+        };
+      }
+
+    static chooseRandomQuote(callback) {
+        //Get a random quote from the list of quotes
+        DBHelper.fetchQuotes((error, quotes) => {
+            let chooseQuoteFunction;
+            if (error) {
+                console.log(error)
+                callback(error, null);
+            } else {
+                chooseQuoteFunction = DBHelper.getRandomQuote(quotes.quotes);
+                callback(chooseQuoteFunction)
+            }
+        });
+
+    }
+}
